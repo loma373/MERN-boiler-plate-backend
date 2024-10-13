4774b3c0f581cd978c5a96d0b64e55379361b2f6
Amol Vyas <amolvyas@Amols-Macbook.local>
2024-10-13 19:58:35 +0530

    adding health check api

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,3 +1,5 @@
+const os = require('os');
+
 const express = require('express');
 
 const auth         = require('./auth');
@@ -11,6 +13,32 @@ router.use('/api/auth', auth);
 router.use('/api/user', user);
 router.use('/api/users', users);
 router.use('/api/todos', todos);
+router.use('/api/health', (req, res) => {
+  const memoryUsage = process.memoryUsage(); // Memory usage of the Node.js process
+  const cpuUsage = process.cpuUsage(); // CPU usage of the Node.js process
+  const uptime = process.uptime(); // Uptime of the Node.js process
+
+  const healthStats = {
+    message: 'Server is up and running...',
+    memoryUsage: {
+      rss: memoryUsage.rss, // Resident Set Size
+      heapTotal: memoryUsage.heapTotal, // Total heap size
+      heapUsed: memoryUsage.heapUsed, // Used heap size
+      external: memoryUsage.external, // Memory usage outside of V8's heap
+    },
+    cpuUsage: {
+      user: cpuUsage.user, // User CPU time in microseconds
+      system: cpuUsage.system, // System CPU time in microseconds
+    },
+    upTime: uptime, // Uptime in seconds
+    platform: os.platform(), // OS platform
+    architecture: os.arch(), // OS architecture
+    totalMemory: os.totalmem(), // Total system memory
+    freeMemory: os.freemem(), // Free system memory
+  };
+
+  res.status(200).send(healthStats);
+});
 
 router.get('/api/tags', (req, res) => {
   res.send([
